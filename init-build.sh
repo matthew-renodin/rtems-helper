e110af86b3139ba83ea934a51fe18394ecee3e27
Matthew Renodin <matthew.renodin@gmail.com>
2019-04-03 10:09:20 -0400

    made some updates to the init-script

--- init-build.sh
+++ init-build.sh
@@ -1,2 +1,30 @@
 
 echo "This is a test"
+
+BUILDDIR=${PWD}
+
+cd rsb
+
+./source-builder/sb-check
+
+cd rtems
+
+../source-builder/sb-set-builder --prefix=$BUILDDIR/rtems/tools 5/rtems-arm
+
+cd $BUILDDIR/kernel
+
+./bootstrap -c && $BUILDDIR/rsb/source-builder/sb-bootstrap
+
+cd $BUILDDIR
+
+mkdir xilinx-zynq-qemu
+
+cd xilinx-zynq-qemu
+
+$BUILDDIR/configure --prefix=$BUILDER/rtems/bsp --target=arm-rtems5 --enable-rtemsbsp=xilinx_zynq_a9_qemu --enable-posix
+
+make
+
+make install
+
+
