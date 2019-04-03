ca3dca75d7ddc8a037aa89032f84ee36b07d5a43
Matthew Renodin <matthew.renodin@gmail.com>
2019-04-03 10:31:29 -0400

    Fixed the configure path to the kernel

--- init-build.sh
+++ init-build.sh
@@ -21,7 +21,7 @@ mkdir xilinx-zynq-qemu
 
 cd xilinx-zynq-qemu
 
-$BUILDDIR/configure --prefix=$BUILDER/rtems/bsp --target=arm-rtems5 --enable-rtemsbsp=xilinx_zynq_a9_qemu --enable-posix
+$BUILDDIR/kernel/configure --prefix=$BUILDER/rtems/bsp --target=arm-rtems5 --enable-rtemsbsp=xilinx_zynq_a9_qemu --enable-posix
 
 make
 
