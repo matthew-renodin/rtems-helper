9459113f6f027c157774fb73095e1b9d89a84cfa
Matthew Renodin <matthew.renodin@gmail.com>
2019-04-04 12:13:10 -0400

    Create init-build.sh

---
+++ init-build.sh
@@ -0,0 +1,5 @@
+# this will be used to generate a modules directory 
+# we will need to list the modules too
+# we will copy the modules from a directory and place them in a directory - 
+# these modules are simply IDL code and C++ code that the IDL will bind with
+
