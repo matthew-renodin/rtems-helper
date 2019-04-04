21c0dbb64c2216e93a2d3389fc42429d6697fcec
Matthew Renodin <matthew.renodin@gmail.com>
2019-04-04 14:07:15 -0400

    Create init-acetao.sh

---
+++ init-acetao.sh
@@ -0,0 +1,10 @@
+
+BUILDDIR=${PWD}
+
+# Build ACE-TAO-mingw-installer
+
+cp projects/ACE-TAO-mingw-installer $BUILDDIR
+
+cp projects/ACE-TAO $BUILDDIR/ACE-TAO-mingw-installer
+
+# Build ACE-TAO-RTEMS-installer
